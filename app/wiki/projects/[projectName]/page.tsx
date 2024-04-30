262bf03ee875bf460ba66ccc22705e38b934c066
Pavel <pbkrutov@gmail.com>
2024-05-01 01:28:35 +0300

    Add edit button

--- app/wiki/projects/[projectName]/page.tsx
+++ app/wiki/projects/[projectName]/page.tsx
@@ -15,19 +15,17 @@ export default function ProjectPage({
 }) {
   return (
     <div className="max-w-8xl mx-auto px-4 lg:px-8 xl:px-12 w-full flex flex-col flex-grow">
-      <div className="hidden md:grid grid-cols-8">
-        <div className="sticky top-20 self-start col-span-3 pr-8 lg:pr-12 py-6 pl-6">
+      <div className="lg:grid grid-cols-8">
+        <div className="lg:sticky lg:top-20 self-start col-span-3 pr-8 lg:pr-12 py-6 pl-6">
           <Button variant={"link"}>
-            <Icons.arrow_left size={18} />{" "}
+            <Icons.arrow_left size={18} />
             <div className={"ms-2"}>Back to Projects</div>
-          </Button>{" "}
+          </Button>
           <br />
           <Badge variant={"outline"} className={"mx-1 mt-5"}>
-            {" "}
             New Project!
           </Badge>
           <Badge variant={"outline"} className={"mx-1"}>
-            {" "}
             Status: Ongoing
           </Badge>
           <h1 className={"text-5xl font-extrabold mt-3"}>
@@ -53,11 +51,31 @@ export default function ProjectPage({
           <div className={"flex grid-cols-2 justify-between"}>
             <p>Project Type:</p> <p>Town</p>
           </div>
+
+          <div className={'mt-7'}>
+            <Button variant={'secondary'}>
+              <Icons.discord weight={'fill'} size={26} className={'mr-2'}/>
+              <p>Log In to Edit</p>
+            </Button>
+          </div>
         </div>
 
-        <div className="col-span-5 border-l overflow-auto pt-8 pb-12 pl-8 lg:pl-12 pr-6">
-          Balh Blah adsad dsadsa dsa dsadsadsafgr gregrtegre gregregre gregregre
-          gregregre
+        <div className="col-span-5 lg:border-l overflow-auto pt-8 pb-12 pl-8 lg:pl-12 pr-6">
+          TipTapEditor class will go here, with content
+          that is auto-got from the DB via a HTTP GET request to NexusCore.
+          The edit button has been added. I will now add a lorem ipsum.<br/><br/>
+
+          Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor. Incididunt ut labore et dolore magna aliqua ut enim ad. Minim veniam quis nostrud exercitation ullamco laboris.
+
+          Nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum sed ut. Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque. Laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae.
+
+          Dicta sunt explicabo nemo enim ipsam voluptatem quia voluptas sit aspernatur aut. Odit aut fugit sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt neque porro quisquam est qui dolorem ipsum quia dolor sit amet consectetur adipisci velit sed quia.
+
+          Non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat. Voluptatem ut enim ad minima veniam quis nostrum exercitationem ullam corporis suscipit laboriosam nisi ut.
+
+          Aliquid ex ea commodi consequatur quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur vel. Illum qui dolorem eum fugiat quo voluptas nulla pariatur at vero.
+
+
         </div>
       </div>
     </div>
