ecb66f8ca4f0705e7cfbfbe081e0a428edf1b15d
Pavel <pbkrutov@gmail.com>
2024-05-01 10:24:08 +0300

    minor content update for testing vercel webhook

--- app/wiki/projects/[projectName]/page.tsx
+++ app/wiki/projects/[projectName]/page.tsx
@@ -71,7 +71,11 @@ export default function ProjectPage({
 
 const content = `<p>TipTapEditor class will go here, with content
 that is auto-got from the DB via a HTTP GET request to NexusCore.
-The edit button has been added. I will now add a lorem ipsum.<br/><br/>
+The edit button has been added. I will now add a lorem ipsum.
+Technically, both the side panel and main content are editable, but
+for projects the side panel will not be edited, and rather auto-generated
+from project info.
+<br/><br/>
 
 Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor. Incididunt ut labore et dolore magna aliqua ut enim ad. Minim veniam quis nostrud exercitation ullamco laboris.
 
@@ -82,4 +86,14 @@ Dicta sunt explicabo nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
 Non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat. Voluptatem ut enim ad minima veniam quis nostrum exercitationem ullam corporis suscipit laboriosam nisi ut.
 
 Aliquid ex ea commodi consequatur quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur vel. Illum qui dolorem eum fugiat quo voluptas nulla pariatur at vero.
+Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor. Incididunt ut labore et dolore magna aliqua ut enim ad. Minim veniam quis nostrud exercitation ullamco laboris.
+
+Nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum sed ut. Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque. Laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae.
+
+Dicta sunt explicabo nemo enim ipsam voluptatem quia voluptas sit aspernatur aut. Odit aut fugit sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt neque porro quisquam est qui dolorem ipsum quia dolor sit amet consectetur adipisci velit sed quia.
+
+Non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat. Voluptatem ut enim ad minima veniam quis nostrum exercitationem ullam corporis suscipit laboriosam nisi ut.
+
+Aliquid ex ea commodi consequatur quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur vel. Illum qui dolorem eum fugiat quo voluptas nulla pariatur at vero.
+
 </p>`
