37cddb199dcf753eec2fdedeab9584d7ab3cba23
cakePhone <mas8042939@gmail.com>
2024-05-05 14:14:21 +0100

    Add various types for the JSON data

--- types/projects.ts
+++ types/projects.ts
@@ -1,12 +1,27 @@
-export interface ProjectJSON {
+import type UserProfile from "./user.ts"
+
+export interface GetProjectResponseJSON {
   project_id: string,
+  name: string,
   coordinates: number[],
   description: string,
-  status: string,
-  content: object,
+  status: number,
+  content: string,
   lead_id: number,
   member_ids: number[],
   thread_id: number,
   accepted_on: string,
-  completed_on: string
+  completed_on: string,
+  public: boolean
+}
+
+export interface ParsedProjectObject {
+	name: string,
+	status: string,
+	coordinates: number[],
+	description: string,
+	content: string,
+	lead_user: UserProfile,
+	members: UserProfile[],
+	started_on: string,
 }
