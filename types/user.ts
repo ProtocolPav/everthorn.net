f176d81fc979f7d863ed43473d594a04b3b98549
Miguel Santos <ms8042939@gmail.com>
2024-05-12 14:51:36 +0100

    make user object type to match provisional object

--- types/user.ts
+++ types/user.ts
@@ -1,4 +1,18 @@
 export interface UserProfile {
-	id: number,
-	nickname: string
+	thorny_id: number,
+	user_id: number,
+	guild_id: number,
+	username: string,
+	join_date: string,
+	birthday: string,
+	balance: number,
+	active: boolean,
+	role: string,
+	patron: boolean,
+	level: number,
+	xp: number,
+	required_xp: number,
+	last_message: string,
+	gamertag: string,
+	whitelist: string
 }
