a0eac59dfba297078e1c2cf27c433d278bf4450a
Miguel Santos <ms8042939@gmail.com>
2024-06-02 13:00:48 +0100

    added parsed projects type

--- types/projects.ts
+++ types/projects.ts
@@ -1,6 +1,6 @@
 import type { UserProfile } from "./user.ts"
 
-export interface ParsedProjectObject {
+export interface ResponseProjectObject {
   project: {
     project_id: string,
     name: string,
@@ -15,6 +15,33 @@ export interface ParsedProjectObject {
   },
   members: {
     members: number[]
+  },
+  status: {
+    status: string,
+    status_since: string,
+  },
+  content: {
+    content: string,
+    content_since: string,
+    content_edited_by: number
+  }
+}
+
+export interface ParsedProjectObject {
+  project: {
+    project_id: string,
+    name: string,
+    description: string,
+    coordinate_x: number,
+    coordinate_y: number,
+    coordinate_z: number,
+    thread_id: number,
+    started_on: string,
+    completed_on: string | null,
+    owner_id: number
+  },
+  members: {
+    members: UserProfile[]
   },
   status: {
     status: string,
