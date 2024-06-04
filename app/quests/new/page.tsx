81f3e4a546a3d8b24418a89327e78a77ed86dde5
Miguel Santos <ms8042939@gmail.com>
2024-06-04 23:56:47 +0100

    add comments and foundation for stepped forms

--- app/quests/new/page.tsx
+++ app/quests/new/page.tsx
@@ -1,8 +1,12 @@
 "use client"
 
+import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { ArrowRight } from "@phosphor-icons/react"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+
+import { cn } from "@/lib/utils"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -16,28 +20,39 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
-import { ArrowRight } from "@phosphor-icons/react"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 const formSchema = z.object({
   title: z
-    .string()
-    .min(2, {
-      message: "MARS!!! Quests need a bigger title.",
-    })
-    .max(50, {
-      message: "MARS...!!! That's a little too much, init?",
-    }),
+    .string({ required_error: "MARS!!! Did you forget to add a title? ;-;" })
+    .min(2, { message: "MARS!!! Quests need a bigger title." })
+    .max(63, { message: "MARS...!!! That's a little too much, init?" }),
   description: z
     .string()
-    .min(10, {
-      message: "MARS!!! People need to know what to do!",
-    })
-    .max(300, {
-      message: "MARS!!! The people have asked for you to stop!",
-    }),
+    .min(10, { message: "MARS!!! People need to know what to do!" })
+    .max(255, { message: "MARS!!! The people have asked for you to stop!" }),
+  objective_type: z.number(),
+  objective_amount: z
+    .number({ required_error: "MARS!!! How many??" })
+    .int({ message: "MARS!!! Fractional numbers??" })
+    .gt(0, { message: "MARS!!! No negative numbers!" }),
+  objective_reward_type: z.string(),
+  objective_reward_amount: z
+    .number()
+    .int({ message: "MARS!!! Fractional numbers??" })
+    .gt(0, { message: "MARS!!! Seriously? People get nothing?" }),
+  objective_reward_item: z.string({ required_error: "MARS!!! What exactly?" }),
+  objective_main_hand: z.string(),
+  location_x: z.number(),
+  location_z: z.number(),
+  radius: z.number().gt(0, { message: "MARS!!! A radius cannot be negative! Learn math!" }),
+  time_limit_h: z.number().int().gt(-1, { message: "MARS!!! No negative time!" }),
+  time_limit_min: z.number().int().gt(-1, { message: "MARS!!! No negative time!" }).lt(61, { message: "MARS!!! How many minutes does an hour have?" })
 })
 
 export default function NewQuest() {
+  const [formStep, setFormStep] = useState(0)
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -55,45 +70,83 @@ export default function NewQuest() {
       <h1 className="text-4xl">Let's create a new quest!</h1>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField
-            control={form.control}
-            name="title"
-            render={({ field }) => (
-              <>
-                <FormItem>
-                  <FormLabel>Title</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Cow Murder"></Input>
-                  </FormControl>
-                  <FormDescription>
-                    This is the quest title, give it a good one!
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              </>
-            )}
-          />
+          <div className={
+            cn({'hidden': formStep !== 0})
+          }>
+            <h2>The Basics</h2>
+            {/* Title */}
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <>
+                  <FormItem className="my-4">
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Cow Murder"></Input>
+                    </FormControl>
+                    <FormDescription>
+                      This is the quest title, give it a good one!
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                </>
+              )}
+            />
 
-          <FormField
-            control={form.control}
-            name="description"
-            render={({ field }) => (
-              <>
-                <FormItem className="my-4">
-                  <FormLabel>Description</FormLabel>
-                  <FormControl>
-                    <Textarea placeholder="Do you like cows? So do I, plea..."></Textarea>
-                  </FormControl>
-                  <FormDescription>
-                    Descriptions should hook people, but shouldn't be so long that it bores them.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              </>
-            )}
-          />
-          
-          <Button variant="outline">Next <ArrowRight className={"ml-1"} size="18"/></Button>
+            {/* Description */}
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <>
+                  <FormItem className="my-4">
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="Do you like cows? So do I, plea..."></Textarea>
+                    </FormControl>
+                    <FormDescription>
+                      Descriptions should hook people, but shouldn't be so long
+                      that it bores them.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                </>
+              )}
+            />
+          </div>
+          <div className={
+            cn({'hidden': formStep !== 1})
+          }>
+            <h2>Objectives</h2>
+            {/* Objective Type */}
+            <FormField
+              control={form.control}
+              name="objective_type"
+              render={({ field }) => (
+                <>
+                  <FormItem className="my-4">
+                    <FormLabel>Objective Type</FormLabel>
+                    <FormControl>
+                      <Select>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Kill"/>
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="kill">Kill</SelectItem>
+                          <SelectItem value="mine">Mine</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                  </FormItem>
+                </>
+              )}  
+            />
+          </div>
+
+          <Button variant="outline">
+            Next <ArrowRight className={"ml-1"} size="18" />
+          </Button>
         </form>
       </Form>
     </section>
