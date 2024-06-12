5462941bdba7c33fa7c1fd141c55627304533d1e
Miguel Santos <ms8042939@gmail.com>
2024-06-12 13:03:27 +0100

    remove callbackURL, no need

--- lib/authActions.ts
+++ lib/authActions.ts
@@ -3,5 +3,5 @@
 import { signIn } from "@/auth"
 
 export async function signInWithDiscord() {
-  await signIn("discord", { callbackUrl: "http://localhost:3000/api/auth/callback/discord" })
+  await signIn("discord")
 }
