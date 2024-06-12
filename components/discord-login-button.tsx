ad1dfa3e4d196a306f3fa647478f7e0f4fe66fa8
Miguel Santos <ms8042939@gmail.com>
2024-06-12 09:48:50 +0100

    use shadcn/ui button for log in

--- components/discord-login-button.tsx
+++ components/discord-login-button.tsx
@@ -1,15 +1,16 @@
 import { signInWithDiscord } from "@/lib/authActions";
 import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button"
 
 export async function SignInButton() {
   return (
     <form
       action={signInWithDiscord}
     >
-      <button type="submit">
+      <Button type="submit" className="flex gap-2">
         <Icons.discord className="size-6" weight="fill" />
-        Signin with Discord
-      </button>
+        Login with Discord
+      </Button>
     </form>
   )
 }
