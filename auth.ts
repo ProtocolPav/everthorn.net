cdfe782bd0ec7d0953561e0ac5c61ca705550def
Miguel Santos <ms8042939@gmail.com>
2024-06-14 09:23:43 +0100

    extend session with jwt strategy

--- auth.ts
+++ auth.ts
@@ -2,5 +2,46 @@ import NextAuth from "next-auth"
 import Discord from "next-auth/providers/discord"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [Discord],
+  providers: [
+    Discord({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      authorization: "https://discord.com/api/oauth2/authorize?scope=identify+email",
+      token: "https://discord.com/api/oauth2/token",
+      userinfo: "https://discord.com/api/users/@me",
+      profile(profile) {
+        return {
+          id: profile.id,
+          name: profile.username,
+          email: profile.email,
+          image: profile.image_url,
+          discriminator: profile.discriminator,
+          verified: profile.verified
+        }
+      }
+    })
+  ],
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+
+      if (profile) {
+        token.id = profile.id;
+        token.discriminator = profile.discriminator;
+        token.verified = profile.verified;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id as string;
+      // @ts-ignore
+      session.user.discriminator = token.discriminator as string;
+      // @ts-ignore
+      session.user.verified = token.verified as boolean;
+      return session;
+    }
+  }
 })
