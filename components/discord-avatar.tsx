382dc8347a3bfa25ea04fce73b90c58e8db6adc2
Miguel Santos <ms8042939@gmail.com>
2024-06-14 16:20:06 +0100

    more eslint bullshit

--- components/discord-avatar.tsx
+++ components/discord-avatar.tsx
@@ -11,6 +11,7 @@ import {
 import { useSession } from "next-auth/react";
 import { signOut } from "next-auth/react";
 import { Check, X, SignOut } from "@phosphor-icons/react";
+import Image from "next/image";
 
 export function DiscordAvatar() {
   const {data: session, status} = useSession()
@@ -39,7 +40,7 @@ export function DiscordAvatar() {
 
       <DropdownMenuTrigger asChild>
         <Button variant="outline" className="gap-2 px-2" onClick={() => console.log("clicked")}>
-          <img src={session?.user?.image || ""} className="aspect-square h-[120%] rounded-full" alt="Avatar"/>
+          <Image src={session?.user?.image || ""} className="aspect-square h-[120%] rounded-full" alt="Avatar"/>
           {session?.user?.nick}
         </Button>
       </DropdownMenuTrigger>
