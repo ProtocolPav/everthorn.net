c3248ac24b042767491e40eb4a18d2534f49838d
Miguel Santos <ms8042939@gmail.com>
2024-06-14 10:08:48 +0100

    make this into an effective token grabber (jk)

--- auth.ts
+++ auth.ts
@@ -4,22 +4,28 @@ import Discord from "next-auth/providers/discord"
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     Discord({
-      clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,
-      authorization: "https://discord.com/api/oauth2/authorize?scope=identify+email",
-      token: "https://discord.com/api/oauth2/token",
-      userinfo: "https://discord.com/api/users/@me",
+      clientId: process.env.AUTH_DISCORD_ID,
+      clientSecret: process.env.AUTH_DISCORD_SECRET,
+      authorization: 'https://discord.com/api/oauth2/authorize?scope=identify+email',
+      token: 'https://discord.com/api/oauth2/token',
+      userinfo: 'https://discord.com/api/users/@me',
       profile(profile) {
-        return {
+        const obj = {
           id: profile.id,
+          nick: profile.global_name ?? profile.username, // Fallback to username if global_name is null
           name: profile.username,
           email: profile.email,
-          image: profile.image_url,
+          image: profile.avatar ? `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` : null,
           discriminator: profile.discriminator,
-          verified: profile.verified
-        }
+          verified: profile.verified,
+          // guilds: profile.guilds // Uncomment and handle if guilds are part of the profile
+        };
+
+        console.log(obj);
+
+        return obj;
       }
-    })
+    }),
   ],
   callbacks: {
     async jwt({ token, account, profile }) {
@@ -29,6 +35,10 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
       if (profile) {
         token.id = profile.id;
+        token.nick = profile.global_name ?? profile.username;
+        token.name = profile.username;
+        token.email = profile.email;
+        token.image = profile.avatar ? `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` : null;
         token.discriminator = profile.discriminator;
         token.verified = profile.verified;
       }
@@ -37,11 +47,13 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
     async session({ session, token }) {
       session.user.id = token.id as string;
-      // @ts-ignore
+      session.user.nick = token.nick as string;
+      session.user.name = token.name as string;
+      session.user.email = token.email as string;
+      session.user.image = token.image as string;
       session.user.discriminator = token.discriminator as string;
-      // @ts-ignore
       session.user.verified = token.verified as boolean;
       return session;
     }
   }
-})
+});
