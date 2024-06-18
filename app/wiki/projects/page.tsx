babc5748b2a5b36f1138db3ea9b2a0c5031e12a0
Miguel Santos <ms8042939@gmail.com>
2024-06-19 00:48:31 +0100

    remove "use client"

--- app/wiki/projects/page.tsx
+++ app/wiki/projects/page.tsx
@@ -1,18 +1,28 @@
 "use client"
 
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { AllProjects } from "@/components/server/all-projects";
+import { Icons } from "@/components/icons"
 
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
 
       <h1>
-        Projects
+        Under Construction!
       </h1>
       <Separator />
-      {/* @ts-expect-error Server Component */}
-      <AllProjects />
+      The new Everthorn Wiki is under construction! Please be patient. <br />
+      Until then, you can use the old wiki.
+
+      <Link href={'https://everthorn.fandom.com/wiki/Category:Projects'}>
+        <Button className={'justify-between'}>
+          <div className={'mr-3'}>View Projects on the Old Wiki </div>
+          <Icons.external_link size={20} />
+        </Button>
+      </Link>
 
     </section>
   )
