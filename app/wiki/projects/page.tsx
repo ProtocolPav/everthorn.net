053d31066ec801bea184d816c224fe312887a50d
Miguel Santos <ms8042939@gmail.com>
2024-06-19 00:48:50 +0100

    remove unused imports

--- app/wiki/projects/page.tsx
+++ app/wiki/projects/page.tsx
@@ -3,6 +3,12 @@
 import { Separator } from "@/components/ui/separator";
 import {AllProjects} from "@/components/server/all-projects";
 
+<<<<<<< HEAD
+=======
+import { Button } from "@/components/ui/button";
+import { Separator} from "@/components/ui/separator";
+import { Icons } from "@/components/icons"
+>>>>>>> e823cae (remove unused imports)
 
 export default function IndexPage() {
   return (
@@ -12,8 +18,20 @@ export default function IndexPage() {
         Projects
       </h1>
       <Separator/>
+<<<<<<< HEAD
       {/* @ts-expect-error Server Component */}
       <AllProjects />
+=======
+      The new Everthorn Wiki is under construction! Please be patient. <br/>
+      Until then, you can use the old wiki.
+
+      <Link href={'https://everthorn.fandom.com/wiki/Category:Projects'}>
+        <Button className={'justify-between'}>
+          <div className={'mr-3'}>View Projects on the Old Wiki </div>
+          <Icons.external_link size={20}/>
+        </Button>
+      </Link>
+>>>>>>> e823cae (remove unused imports)
 
     </section>
   )
