6b36ae9d80186da0f917bf42081ae5fcf2a851a7
Miguel Santos <ms8042939@gmail.com>
2024-06-19 00:48:50 +0100

    remove unused imports

--- app/wiki/projects/page.tsx
+++ app/wiki/projects/page.tsx
@@ -1,23 +1,21 @@
 "use client"
 import Link from "next/link"
 
-import { siteConfig } from "@/config/site";
-import { buttonVariants, Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { Separator} from "@/components/ui/separator";
 import { Icons } from "@/components/icons"
 
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      
+
       <h1>
         Under Construction!
       </h1>
       <Separator/>
       The new Everthorn Wiki is under construction! Please be patient. <br/>
       Until then, you can use the old wiki.
-      
+
       <Link href={'https://everthorn.fandom.com/wiki/Category:Projects'}>
         <Button className={'justify-between'}>
           <div className={'mr-3'}>View Projects on the Old Wiki </div>
