0bd745250cf7e3ed320105db6cae4960566da595
Miguel Santos <ms8042939@gmail.com>
2024-06-19 00:48:50 +0100

    remove unused imports

--- app/wiki/projects/page.tsx
+++ app/wiki/projects/page.tsx
@@ -3,7 +3,6 @@
 import { Separator } from "@/components/ui/separator";
 import { AllProjects } from "@/components/server/all-projects";
 
-
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
