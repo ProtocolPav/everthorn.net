0102203487e5f2860269f4b61ee4857c698a6fe8
Miguel Santos <ms8042939@gmail.com>
2024-06-19 15:25:32 +0100

    Finish base for discord profile view

--- components/client/discord-profile.tsx
+++ components/client/discord-profile.tsx
@@ -16,26 +16,52 @@ export function DiscordProfile({ profile }: DiscordProfileProps) {
   console.log(profile)
 
   return (
-    <div className="mb-16">
+    <div className="mb-4">
       {/* Top banner and profile picture */}
       <div
-        className={ cn("relative h-22 w-full rounded-sm", { "h-11": !profile.banner })}
-        style={
+        className={ cn("relative h-24 w-full rounded-sm mb-10", { "h-14": !profile.banner })}
+        style={{ backgroundColor: profile.banner_color as string }}
+      >
         {
-          backgroundImage: profile.banner,
-          backgroundColor: profile.banner_color as string,
-        }
-      }>
-        <img
-          src={(profile.banner) ? profile.banner : undefined}
-          alt="Profile banner"
+          (profile.banner)
+            ? (
+              <img
+                src={profile.banner}
+                alt="Profile banner"
 
-          className="h-full w-full rounded-sm object-cover"
-        />
+                className="h-full w-full rounded-sm object-cover"
+              />
+            )
+            : undefined
+        }
+
         {/* Profile picture with decoration */}
-        <div className="absolute -bottom-8 left-4 size-16">
-          <img src={profile.image as string} alt="User profile picture." className="rounded-full outline outline-4 outline-background"/>
+        <div className="absolute -bottom-11 left-0 size-[5.5rem]">
+          <img
+            src={profile.image as string}
+            alt="User profile picture."
+
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full size-16 outline outline-4 outline-background"
+          />
+          {
+            (profile.decoration)
+              ? (
+                <img
+                  src={profile.decoration as string}
+                  alt=""
+
+                  className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-[5.5rem]"
+                />
+              )
+              : undefined
+          }
         </div>
+      </div>
+
+      {/* Nickname and username */}
+      <div className="mx-3">
+        <p className="text-lg m-0"><b>{ profile.nick }</b></p>
+        <p className="text-sm m-0 -mt-1">{ profile.name }</p>
       </div>
     </div>
   )
