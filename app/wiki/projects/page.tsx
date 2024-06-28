1033fa78ad9f4ecd9e8a1fb7ffe44dc553fd999e
Miguel Santos <ms8042939@gmail.com>
2024-06-28 11:30:51 +0100

    solve conflicts

--- app/wiki/projects/page.tsx
+++ app/wiki/projects/page.tsx
@@ -3,13 +3,6 @@
 import { Separator } from "@/components/ui/separator";
 import {AllProjects} from "@/components/server/all-projects";
 
-<<<<<<< HEAD
-=======
-import { Button } from "@/components/ui/button";
-import { Separator} from "@/components/ui/separator";
-import { Icons } from "@/components/icons"
->>>>>>> e823cae (remove unused imports)
-
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
@@ -18,20 +11,8 @@ export default function IndexPage() {
         Projects
       </h1>
       <Separator/>
-<<<<<<< HEAD
       {/* @ts-expect-error Server Component */}
       <AllProjects />
-=======
-      The new Everthorn Wiki is under construction! Please be patient. <br/>
-      Until then, you can use the old wiki.
-
-      <Link href={'https://everthorn.fandom.com/wiki/Category:Projects'}>
-        <Button className={'justify-between'}>
-          <div className={'mr-3'}>View Projects on the Old Wiki </div>
-          <Icons.external_link size={20}/>
-        </Button>
-      </Link>
->>>>>>> e823cae (remove unused imports)
 
     </section>
   )
