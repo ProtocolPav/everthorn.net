86adea2ccc423aa684a9a834f1d9cbaf707b6d05
Miguel Santos <ms8042939@gmail.com>
2024-07-06 16:06:11 +0100

    link project cards to their respective pages

--- components/client/project-card.tsx
+++ components/client/project-card.tsx
@@ -1,17 +1,20 @@
 import { Project } from "@/types/projects";
 import {Card, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import Link from "next/link";
 
 interface projectCardProps {
-  project: Project
+    project: Project,
 }
 
 export function ProjectCard({ project }: projectCardProps) {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{project.project.name}</CardTitle>
-        <CardDescription>{project.project.description}</CardDescription>
-      </CardHeader>
-    </Card>
+      <Card>
+        <Link href={`/wiki/projects/${project.project.project_id}`}>
+          <CardHeader>
+            <CardTitle>{project.project.name}</CardTitle>
+            <CardDescription>{project.project.description}</CardDescription>
+          </CardHeader>
+        </Link>
+      </Card>
   )
 }
