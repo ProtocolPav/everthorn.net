7fe6749daa0ec07a48574e74cbff401175c5fcdc
cakePhone <ms8042939@gmail.com>
2024-07-19 12:15:21 +0100

    Restructure schema to allow arrays and checks

--- lib/forms/new_quest.ts
+++ lib/forms/new_quest.ts
@@ -1,45 +1,22 @@
 import { z } from "zod"
 
-export const formSchema = z.object({
-  title: z
-    .string({ required_error: "MARS!!! Did you forget to add a title? ;-;" })
-    .min(2, { message: "MARS!!! Quests need a bigger title." })
-    .max(63, { message: "MARS...!!! That's a little too much, init?" }),
-  description: z
-    .string()
-    .min(10, { message: "MARS!!! People need to know what to do!" })
-    .max(255, { message: "MARS!!! The people have asked for you to stop!" }),
-
-
-  objective_type: z.string({ required_error: "MARS!!! What do the people need to do?" }),
-  objective_amount: z.preprocess(
+export const objectiveSchema = z.object({
+  type: z.string({ required_error: "MARS!!! What do the people need to do?" }),
+  amount: z.preprocess(
     (value) => (typeof value === "string") ? Number(value) : 0,
     z.number({ required_error: "MARS!!! How many??" })
-    .int({ message: "MARS!!! Fractional numbers??" })
-    .min(1, { message: "MARS!!! People can't just sit back and relax!" })
+      .int({ message: "MARS!!! Fractional numbers??" })
+      .min(1, { message: "MARS!!! People can't just sit back and relax!" })
   ),
-  objective_item: z
+  mob_block: z
     .string({ required_error: "MARS!!! What do people need to kill or mine?" })
     .toLowerCase(),
-
-
-  objective_reward_type: z.string({ required_error: "MARS!!! What kind of reward?!" }),
-  objective_reward_amount: z.preprocess(
-    (value) => (typeof value === "string") ? Number(value) : 0,
-    z.number()
-    .int({ message: "MARS!!! Fractional numbers??" })
-    .min(1, { message: "MARS!!! Seriously? People get nothing?" })
-  ),
-  objective_reward_item: z
-    .string()
-    .optional(),
-
 
   require_main_hand: z
     .boolean()
     .default(false)
     .optional(),
-  objective_main_hand: z
+  main_hand: z
     .string()
     .optional(),
   require_location: z
@@ -74,18 +51,82 @@ export const formSchema = z.object({
     (value) => (typeof value === "string") ? Number(value) : 0,
     z.number().int().min(0, { message: "MARS!!! No negative time!" }).max(59, { message: "MARS!!! How many seconds does a minute have?" }).optional()
   )
+})
+
+export const rewardSchema = z.object({
+  type: z.string({ required_error: "MARS!!! What kind of reward?!" }),
+  amount: z.preprocess(
+    (value) => (typeof value === "string") ? Number(value) : 0,
+    z.number()
+      .int({ message: "MARS!!! Fractional numbers??" })
+      .min(1, { message: "MARS!!! Seriously? People get nothing?" })
+  ),
+  item: z
+    .string()
+    .optional(),
+})
+
+export const formSchema = z.object({
+  title: z
+    .string({ required_error: "MARS!!! Did you forget to add a title? ;-;" })
+    .min(2, { message: "MARS!!! Quests need a bigger title." })
+    .max(63, { message: "MARS...!!! That's a little too much, init?" }),
+  description: z
+    .string()
+    .min(10, { message: "MARS!!! People need to know what to do!" })
+    .max(255, { message: "MARS!!! The people have asked for you to stop!" }),
+
+  objectives: objectiveSchema.array(),
+
+  rewards: rewardSchema.array()
+
 }).refine(data => {
-  return data?.objective_reward_type === "item" && data?.objective_reward_item !== "" || data?.objective_reward_type === "balance";
+  for (let i = 0; i < data?.rewards.length; i++) {
+    const reward = data?.rewards[i]
+
+    const check: boolean = (
+      reward.type === "item" &&
+      reward.item !== "" ||
+      reward.type === "balance"
+    )
+    if (!check) return false
+  }
 }, {
   message: "MARS!!! You forgot to specify an item to reward!",
   path: ["objective_reward_item"]
 }).refine(data => {
-  return (!data?.require_main_hand || (data?.require_main_hand && typeof data?.objective_main_hand !== "undefined"))
+  for (let i = 0; i < data?.objectives.length; i++) {
+    const objective = data?.objectives[i]
+
+    const check = (
+      !objective.require_main_hand ||
+      (
+        objective.require_main_hand &&
+        typeof objective.main_hand !== "undefined"
+      )
+    )
+    if (!check) return false
+  }
+
+  // return (!data?.require_main_hand || (data?.require_main_hand && typeof data?.objective_main_hand !== "undefined"))
 }, {
   message: "MARS!!! You forgot to specify the required main hand item!",
   path: ["objective_main_hand"]
 }).refine(data => {
-  return (!data?.require_location || (data?.require_location && typeof data?.location_x !== "undefined" && typeof data?.location_z !== "undefined" && typeof data?.radius !== "undefined"))
+  for (let i = 0; i < data?.objectives.length; i++) {
+    const objective = data?.objectives[i]
+
+    const check = (
+      !objective.require_location ||
+      (
+        objective.require_location &&
+        typeof objective.location_x !== "undefined" &&
+        typeof objective.location_z !== "undefined" &&
+        typeof objective.radius !== "undefined"
+      )
+    )
+    if (!check) return false
+  }
 }, {
   message: "MARS!!! You forgot to specify the coordinates!",
   path: ["require_location"]
