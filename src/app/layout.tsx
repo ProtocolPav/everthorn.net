0a2cad59798d8d925e631af98bc6d287f0367fed
Pavel <pbkrutov@gmail.com>
2024-12-24 15:26:07 +0200

    remove icons.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,6 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
 }
