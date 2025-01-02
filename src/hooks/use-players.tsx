b8ca9ff2a9ce3a4e42398d7d5adc67e4d6e6b9aa
Pavel <pbkrutov@gmail.com>
2025-01-02 18:34:48 +0200

    change to use nexuscore api

--- src/hooks/use-players.tsx
+++ src/hooks/use-players.tsx
@@ -9,7 +9,7 @@ export interface Player {
 export function usePlayers(): {players: Player[], isError: any, isLoading: boolean} {
     const fetcher = (...args: [any]) => fetch(...args).then((res) => res.json());
 
-    const { data, error, isLoading } = useSWR("/localapi/api/v0.1/server/players", fetcher, {refreshInterval: 1000});
+    const { data, error, isLoading } = useSWR("/nexuscore-api/v0.1/server/players", fetcher, {refreshInterval: 1000});
 
     return {
         players: data,
