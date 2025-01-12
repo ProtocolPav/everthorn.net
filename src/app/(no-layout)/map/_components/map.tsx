bf09b6e05b637e3a3796efa3cbbe95e77f704e16
Pavel <pbkrutov@gmail.com>
2025-01-12 18:15:23 +0200

    change maxNativeZoom for map to 2

--- src/app/(no-layout)/map/_components/map.tsx
+++ src/app/(no-layout)/map/_components/map.tsx
@@ -37,7 +37,7 @@ const CustomTileLayerComponent = () => {
     React.useEffect(() => {
         // Add the custom Tile Layer to the map
         const customTileLayer = new CustomTileLayer("", {
-            maxNativeZoom: 3,
+            maxNativeZoom: 2,
             maxZoom: 6,
             minZoom:-5,
             updateInterval:10,
