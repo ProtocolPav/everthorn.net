f867952e36e21118933a5bd9564e0d139853c9dd
Pavel <pbkrutov@gmail.com>
2025-01-19 13:57:39 +0200

    update map control

--- src/app/(no-layout)/map/_components/controls/index.tsx
+++ src/app/(no-layout)/map/_components/controls/index.tsx
@@ -1,5 +1,5 @@
 import {CoordinatesControl} from "./coords-control";
-import {Card, CardContent} from "@/components/ui/card";
+import {Card, CardContent, CardFooter} from "@/components/ui/card";
 import logo from '/public/everthorn.png';
 import Image from "next/image";
 import Link from "next/link";
@@ -16,7 +16,7 @@ export default function ControlBar (
     {pins: Toggle[], update_pins: Function, layers: Toggle[], update_layers: Function}) {
     return (
         <Control position={'topleft'}>
-            <div className={'flex gap-2 items-center'}>
+            <div className={'flex items-center gap-2'}>
                 <Card className={'bg-background/60 backdrop-blur-sm'}>
                     <CardContent className={'flex gap-1 p-1'}>
                         <Button variant={'outline'} size={'icon'} className={'bg-background/60'}>
@@ -32,12 +32,11 @@ export default function ControlBar (
 
                         <PinsControl pins={pins} update_pins={update_pins}/>
                     </CardContent>
+                    <CardFooter className={'flex justify-center gap-1 rounded-b-md bg-white/20 px-0 py-0.5 text-[10px]'}>
+                        <b className={'rounded-full bg-attention px-1'}>BETA</b>
+                        May not work as expected
+                    </CardFooter>
                 </Card>
-
-                <div className={'grid grid-cols-1 gap-1'}>
-                    <Badge variant={'attention'} className={'mx-auto'}>BETA</Badge>
-                    <Badge variant={'destructive'} className={'flex gap-0.5'}><Circle width={10} weight={'fill'}/>Live</Badge>
-                </div>
 
             </div>
 
