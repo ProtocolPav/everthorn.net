f007325add17864c245ca8321fa3d6fa2d43bd6a
Pavel <pbkrutov@gmail.com>
2025-03-06 15:37:30 +0200

    fix admin dashboard layout

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import {SidebarInset, SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar"
 import {AdminSidebar} from "@/app/(admin)/admin/_components/admin-sidebar"
 import {Separator} from "@/components/ui/separator";
 import React from "react";
@@ -23,17 +23,18 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
             <AdminSidebar />
-            <main>
-                <header
-                    className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+            <SidebarInset className={'bg-transparent'}>
+                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
                     <div className="flex items-center gap-2 px-4">
                         <SidebarTrigger className="-ml-1"/>
                         <Separator orientation="vertical" className="mr-2 h-4"/>
                         <h3 className={'text-sm'}>Everthorn Admin Dashboard</h3>
                     </div>
                 </header>
-                {children}
-            </main>
+                <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                    {children}
+                </div>
+            </SidebarInset>
         </SidebarProvider>
     )
 }
