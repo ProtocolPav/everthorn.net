c726d085ca85ada22f11cb82cabc3303c2a519ec
Pavel <pbkrutov@gmail.com>
2025-06-14 16:33:23 +0300

    fix typing

--- src/hooks/use-thorny-user.ts
+++ src/hooks/use-thorny-user.ts
@@ -1,8 +1,41 @@
-// hooks/use-thorny-user.ts
+// hooks/useUser.ts
 import useSWR from 'swr';
 
 export interface ThornyUser {
-    // ... your existing interface
+    username: string;
+    birthday: string;
+    balance: number;
+    active: boolean;
+    role: string;
+    patron: boolean;
+    level: number;
+    xp: number;
+    required_xp: number;
+    last_message: string;
+    gamertag: string;
+    whitelist: string;
+    thorny_id: number;
+    user_id: string;
+    guild_id: string;
+    join_date: string;
+    profile: {
+        slogan: string;
+        aboutme: string;
+        lore: string;
+        character_name: string;
+        character_age: number;
+        character_race: string;
+        character_role: string;
+        character_origin: string;
+        character_beliefs: string;
+        agility: number;
+        valor: number;
+        strength: number;
+        charisma: number;
+        creativity: number;
+        ingenuity: number;
+        thorny_id: number;
+    };
 }
 
 const fetcher = async (url: string): Promise<ThornyUser> => {
