012c1d241814da599ba39404d87ba7c48639439b
Pavel <pbkrutov@gmail.com>
2025-08-17 11:40:19 +0300

    Add reverse proxy for amethyst.everthorn.net in nginx
    Configured nginx to proxy requests for amethyst.everthorn.net to a Portainer service. Removed redundant route handling for Portainer in next.config.js to streamline routing through the reverse proxy.

--- next.config.js
+++ next.config.js
@@ -33,10 +33,6 @@ const nextConfig = {
       {
         source: '/amethyst/:path*',
         destination: 'http://amethyst:8000/:path*'
-      },
-      {
-        source: '/admin/portainer/:path*',
-        destination: 'https://portainer:9443/:path*'
       }
     ]
   }
